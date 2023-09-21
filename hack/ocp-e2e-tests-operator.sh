48f645dc49848a058497dc1b252c37dba94c70f9
Mat Kowalski <mko@redhat.com>
2023-09-21 10:03:09 +0200

    UPSTREAM: <carry>: Fix CI SSH proxy hack
    After moving from Packet to OFCIR we don't need to source UID hacks
    anymore.

--- hack/ocp-e2e-tests-operator.sh
+++ hack/ocp-e2e-tests-operator.sh
@@ -17,7 +17,6 @@ export KUBEVIRTCI_RUNTIME="${KUBEVIRTCI_RUNTIME:-podman}"
 export FLAKE_ATTEMPTS="${FLAKE_ATTEMPTS:-3}"
 
 if [ "${CI}" == "true" ]; then
-    source ${SHARED_DIR}/fix-uid.sh
     export SSH=./hack/ssh-ci.sh
 else
     export SSH=./hack/ssh.sh
